1d9ba5d08879bed59f9635a88a757512d5b418e9
alaa140101 <alaa140011@gmail.com>
2020-09-30 08:35:37 +0300

    Music Player With JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+  {
+    name: 'quran-1',
+    diaplayName: 'Al-Rahman',
+    artist: 'Edres Abkar',
+  },
+  {
+    name: 'quran-2',
+    diaplayName: 'Maram',
+    artist: 'Anas Abkar',
+  },
+  {
+    name: 'quran-3',
+    diaplayName: 'Fater',
+    artist: 'Yaser Abkar',
+  },
+  {
+    name: 'quran-4',
+    diaplayName: 'Yseen',
+    artist: 'Ali Abkar',
+  }
+];
 
 // Initalize play
 let isPlaying = false;
@@ -24,3 +51,40 @@ function pauseSong() {
 
 // Play or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM 
+function loadSong(song) {
+  title.textContent = song.diaplayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// Previous Song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length -1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+// Next Song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length-1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// On Load - Select First Song
+loadSong(songs[2]);
+
+//Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
